a7bac7a435ca76eeba60ee74e61f2228cb3923d5
gabrielsanttos <gabrielconceicao827@gmail.com>
2026-04-12 14:39:23 -0400

    mexi umas paradas ai

--- questao03.c
+++ questao03.c
@@ -7,11 +7,17 @@
 
 int main(){
     srand(time(NULL));
+    setlocale(LC_ALL,"pt_BR.UTF8");
+
     int vet[100000];
+    struct timespec inicio, fim;
+
     criaVetordeIntDesordenado(vet,100000);
-   
+    
+    clock_gettime(CLOCK_REALTIME,&inicio);
     bubbleSort(vet,100000);
-    for(int i=0;i<100000;i++){
-        printf("[%d] ",vet[i]);
-    }
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução bubble sort: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+
 }
