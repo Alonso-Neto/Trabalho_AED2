dfc9571e00903903303cd983b8139dd4173d3e52
gabrielsanttos <gabrielconceicao827@gmail.com>
2026-04-18 10:12:21 -0400

    Resolver conflito de merge

--- questao03.c
+++ questao03.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <math.h>
+#include <time.h>
 #include "TAD-Vetorzao.h"
 
 int main(){
@@ -10,14 +10,53 @@ int main(){
     setlocale(LC_ALL,"pt_BR.UTF8");
 
     int vet[100000];
+    int vetbase[100000];
     struct timespec inicio, fim;
 
-    criaVetordeIntDesordenado(vet,100000);
+    criaVetordeIntDesordenado(vetbase,100000);
+
+    copiaVetor(vetbase, vet, 100000);
     
     clock_gettime(CLOCK_REALTIME,&inicio);
     bubbleSort(vet,100000);
     clock_gettime(CLOCK_REALTIME,&fim);
     printf("Tempo de execução bubble sort: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+    copiaVetor(vetbase, vet, 100000);
+
+    clock_gettime(CLOCK_REALTIME,&inicio);
+    insercaoDireta(vet,100000);
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução inserção direta: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+    copiaVetor(vetbase, vet, 100000);
+
+    clock_gettime(CLOCK_REALTIME,&inicio);
+    selecaoDireta(vet,100000);
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução seleção Direta: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+    copiaVetor(vetbase, vet, 100000);
+
+    clock_gettime(CLOCK_REALTIME,&inicio);
+    quicksort(vet,0, 100000-1);
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução quicksort: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+    copiaVetor(vetbase, vet, 100000);
+
+    clock_gettime(CLOCK_REALTIME,&inicio);
+    merge_sort(vet, 100000-1);
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução merge sort: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));
+
+    copiaVetor(vetbase, vet, 100000);
+
+    /*clock_gettime(CLOCK_REALTIME,&inicio);
+    bogoSort(vet,100000);
+    clock_gettime(CLOCK_REALTIME,&fim);
+    printf("Tempo de execução bogo sort: %.6lf milissegundos\n",tempoDeExecucao(inicio,fim));*/
+
 
 
 }
